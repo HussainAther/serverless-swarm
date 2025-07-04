c3f74008a49a06673661ac5abd8b0176a34f0bde
Syed Hussain Ather <shussainather@gmail.com>
2025-07-04 11:07:27 -0400

    init

---
+++ templates/rust/src/main.rs
@@ -0,0 +1,11 @@
+use lambda_http::{run, service_fn, Body, Error, Request, Response};
+
+async fn function_handler(_event: Request) -> Result<Response<Body>, Error> {
+    Ok(Response::new(\"Hello from Rust Lambda!\".into()))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    run(service_fn(function_handler)).await
+}
+
